10ce26ecff24590619151b92d36f8a7b6f5eab28
ayushijadhav2006 <124800578+ayushijadhav2006@users.noreply.github.com>
2025-03-22 13:18:46 +0530

    installation

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['firebasestorage.googleapis.com'], // Add the domain here
+    },
+};
+
+export default nextConfig;
